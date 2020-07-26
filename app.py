a2033ac29161a0026131b60bf449e0a6bb3cb91d
Siroyan <1517053@ed.tus.ac.jp>
2020-07-26 15:21:37 +0000

    [add] s3 bucket setting

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deploy
+)
+import os
 
 from tenbo_online.tenbo_online_stack import TenboOnlineStack
 
 
 app = core.App()
-TenboOnlineStack(app, "tenbo-online")
+TenboOnlineStack(
+    app, "tenbo-online",
+    env = {
+        "region": os.environ["CDK_DEFAULT_REGION"],
+        "account": os.environ["CDK_DEFAULT_ACCOUNT"]
+    }
+)
 
 app.synth()
--- tenbo_online/tenbo_online_stack.py
+++ tenbo_online/tenbo_online_stack.py
@@ -1,9 +1,25 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_s3_deployment as aws_s3_deploy
+)
+import os
 
 class TenboOnlineStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(
+            self, "tenbo-online.io"",
+            website_index_document = "index.html",
+            public_read_access = True,
+            removal_policy = core.RemovalPolicy.DESTROY
+        )
+
+        s3_deploy.BucketDeployment(
+            self, "BucketDeployment",
+            destination_bucket = bucket,
+            sources = [s3_deploy.Source.asset("./contents")],
+            retain_on_delete = False,
+        )
